9d808157cf70cd77f28d6b4763b4b8ea5f183247
ykermarec <Nheqtrov22>
2020-01-14 11:20:47 +0100

    Add script to check connexion and lunch reboot

---
+++ checkConnexion.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+NOW=$(date)
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+  # echo $NOW" - IPv4 is up" >> /var/log/error_check_connexion.log
+  echo "up"
+else
+  echo "down"
+  # echo $NOW" - IPv4 is down" >> /var/log/error_check_connexion.log
+  sleep 2m
+  if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+    echo "reup"
+    # echo $NOW" - IPv4 is re-up" >> /var/log/error_check_connexion.log
+  else
+    echo "reboot"
+    # echo $NOW" - REBOOT FREEBOX !!!!" >> /var/log/error_check_connexion.log
+    ./reboot-fbx/reboot-fbx.sh
+  fi
+fi
+
